0e28eaad6e560a897fbef1d64dc80df521644e47
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-02 00:49:02 +0530

    MVC Architecture

---
+++ Lecture 13 - MVC Architecture/project - airbnb/controllers/hostController.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const rootDir = require('../utils/pathUtil');
+const Home = require('../models/home');
+
+// Host
+exports.getAddHome = (req, res, next) =>{
+  res.sendFile(path.join(rootDir, 'views', 'host/addHome.html'));
+};
+
+exports.postAddHome = (req, res, next) =>{
+  const { houseName, price, location, rating, photoUrl } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
+  home.save();
+  res.sendFile(path.join(rootDir, 'views', 'host/homeAdded.html'));
+}
+
+exports.getEditHome = (req, res, next) =>{
+  res.sendFile(path.join(rootDir, 'views', 'host/edit-Home.html'));
+};
+
+exports.getHostHomeList = (req, res, next) =>{
+  res.sendFile(path.join(rootDir, 'views', 'host/host-home-list.html'));
+};
